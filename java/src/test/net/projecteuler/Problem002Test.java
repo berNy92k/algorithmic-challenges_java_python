2f8935e76054c6c0e1b00899b61d3109c6c598a7
dbernacki <daw.bernacki@gmail.com>
2026-02-15 22:44:10 +0100

    Add logic for Problem002 + tests.

---
+++ java/src/test/net/projecteuler/Problem002Test.java
@@ -0,0 +1,40 @@
+package net.projecteuler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Problem002Test {
+
+    private Problem002 problem;
+
+    @BeforeEach
+    void setUp() {
+        problem = new Problem002();
+    }
+
+    @Test
+    void sumOfAllFibonacciValuesForMaxLastValue_shouldReturnCorrectValue_whenLastValueIsMax100() {
+        // given
+        final int maxValue = 100;
+
+        // when
+        final long sum = problem.sumOfAllFibonacciValuesForMaxLastValue(maxValue);
+
+        // then
+        assertEquals(44, sum);
+    }
+
+    @Test
+    void sumOfAllFibonacciValuesForMaxLastValue_shouldReturnCorrectValue_whenLastValueIsMax4mln() {
+        // given
+        final int maxValue = 4000000;
+
+        // when
+        final long sum = problem.sumOfAllFibonacciValuesForMaxLastValue(maxValue);
+
+        // then
+        assertEquals(4613732, sum);
+    }
+}

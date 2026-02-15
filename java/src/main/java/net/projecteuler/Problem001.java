bbb49ee382d73d944e5a52af829ff0fffb5551be
dbernacki <daw.bernacki@gmail.com>
2026-02-15 22:58:44 +0100

    Add logic for Problem002 + tests.

--- java/src/main/java/net/projecteuler/Problem001.java
+++ java/src/main/java/net/projecteuler/Problem001.java
@@ -2,8 +2,6 @@ package net.projecteuler;
 
 import java.util.stream.IntStream;
 
-// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
-// Find the sum of all the multiples of 3 or 5 below 1000.
 public class Problem001 {
 
     public long sumOfAllMultiples(final int maxValue, final int multipleA, final int multipleB) {
--- python/src/euler/problem001.py
+++ python/src/euler/problem001.py
@@ -1,6 +1,3 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
-
 def sum_of_all_multiples(max_value, multiple_a, multiple_b):
     sum = 0
     for nb in range(1, max_value):

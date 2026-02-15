41c1506e458896052f53d63d81a2620ac60aa661
dbernacki <daw.bernacki@gmail.com>
2026-02-15 22:02:02 +0100

    Add logic for Problem001 + tests.

--- java/src/main/java/net/projecteuler/Problem001.java
+++ java/src/main/java/net/projecteuler/Problem001.java
@@ -3,4 +3,15 @@ package net.projecteuler;
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 public class Problem001 {
+
+    public long sumOfAllMultiples(final int maxValue, final int multipleA, final int multipleB) {
+        int sum = 0;
+        for (int i = 1; i < maxValue; i++) {
+            if (i % multipleA == 0 || i % multipleB == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
 }
--- java/src/test/net/projecteuler/Problem001Test.java
+++ java/src/test/net/projecteuler/Problem001Test.java
@@ -1,7 +1,44 @@
 package net.projecteuler;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Problem001Test {
 
+    private Problem001 problem;
+
+    @BeforeEach
+    void setUp() {
+        problem = new Problem001();
+    }
+
+    @Test
+    void sumOfAllMultiples_shouldReturnCorrectValue_whenMultipleOf3And5ForNumbersBelow10() {
+        // given
+        final int maxValue = 10;
+        final int multipleA = 3;
+        final int multipleB = 5;
+
+        // when
+        final long sum = problem.sumOfAllMultiples(maxValue, multipleA, multipleB);
+
+        // then
+        assertEquals(23, sum);
+    }
+
+    @Test
+    void sumOfAllMultiples_shouldReturnCorrectValue_whenMultipleOf3And5ForNumbersBelow1000() {
+        // given
+        final int maxValue = 1000;
+        final int multipleA = 3;
+        final int multipleB = 5;
+
+        // when
+        final long sum = problem.sumOfAllMultiples(maxValue, multipleA, multipleB);
+
+        // then
+        assertEquals(233168, sum);
+    }
 }
